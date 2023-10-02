ee128f5506bae98c35e889f4e226391993d5d29a
GIHAA <gihansudeeparandimal@gmail.com>
2023-10-02 06:03:12 +0530

    Initialize project

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomTabNavigation from "./navigation/BottomTabNavigation";
+import Home from "./components/screens/Home";
+import MyCart from './components/screens/MyCart';
+import ProductInfo from './components/screens/ProductInfo';
+import { db } from "./firebase.config";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Bottom Navigation"
+          component={BottomTabNavigation}
+          options={{ headerShown: false }}
+        />
+         <Stack.Screen name="Home" component={React.memo(Home)} options={{ headerShown: false }} />
+        <Stack.Screen name="MyCart" component={MyCart} options={{ headerShown: false }}/>
+        <Stack.Screen name="ProductInfo" component={ProductInfo} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ navigation/BottomTabNavigation.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
+import { COLORS } from "../constants/index";
+import { Entypo } from '@expo/vector-icons';
+import Home from "../components/screens/Home"
+
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions = {
+  tabBarShowLabel: false,
+  tabBarHideOnKeyboard: true,
+  headerShown: false,
+  tabBarStyle: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 70,
+  },
+};
+
+const BottomTabNavigation = () => {
+  return (
+    <Tab.Navigator screenOptions={screenOptions}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={React.memo(Home)}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={"search-sharp"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="scan"
+        component={React.memo(Home)}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <View style={{ backgroundColor: focused ? COLORS.primary : "#198155", padding: 18 , borderRadius: 50 }}>
+              <Entypo
+                name="camera"
+                size={24}
+                color="white"
+              />
+              </View>
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="fav"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+
+              <Feather
+                name={"heart"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+
+      <Tab.Screen
+        name="profile"
+        component={React.memo(Home)}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name={focused ? "person" : "person-outline"}
+                size={24}
+                color={focused ? COLORS.primary : COLORS.gray2}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigation;
