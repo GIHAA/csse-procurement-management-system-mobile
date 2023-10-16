1bf670222ebd0406caa5998684b31cda9055d296
GIHAA <gihansudeeparandimal@gmail.com>
2023-10-16 22:40:11 +0530

    refat: change image location

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,14 @@
 module.exports = {
-    // ... other Jest configuration options ...
-  
-    // Ensure that Jest can handle ES6 modules.
-    transform: {
-      '^.+\\.jsx$': 'babel-jest',
-    },
-  
-    // Define the file extensions you want Jest to recognize.
-    moduleFileExtensions: ['js', 'jsx'],
-  
-    // You might need to configure moduleNameMapper for assets or styles, if applicable.
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-  
-    // Set up Jest to work with ES6 modules.
-    testEnvironment: 'jest-environment-node',
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-  };
-  
-  // ... other Jest configuration options ...
-  
+  transform: {
+    "^.+\\.jsx$": "babel-jest",
+  },
+
+  moduleFileExtensions: ["js", "jsx"],
+
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+
+  testEnvironment: "jest-environment-node",
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+};
